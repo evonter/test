1ecddb2a6e6d34dd67af9eda8c0069ded5b864ce
evonter <124935416+evonter@users.noreply.github.com>
2023-02-10 00:26:46 +0300

    Add files via upload

---
+++ lang/en.php
@@ -0,0 +1,20 @@
+<?php 
+$data['title'] = 'Request form';
+
+$data['h1'] = 'Request form';
+
+$data['label_name'] = 'Your Name';
+$data['label_email'] = 'Your Email';
+$data['label_phone'] = 'Your Telephone';
+
+$data['button_send'] = 'Send';
+
+$data['error_name'] = 'Enter your Name';
+$data['error_email'] = 'Enter you Email correctly';
+$data['error_phone'] = 'Enter you Telephone correctly';
+$data['error_email_exist'] = 'Email is already exist';
+$data['error_phone_exist'] = 'Telephone is already exist';
+$data['error_request'] = 'Request error. Try again later';
+
+$data['success'] = 'Your request have been sended!';
+
---
+++ lang/ru.php
@@ -0,0 +1,20 @@
+<?php 
+$data['title'] = 'Форма заявки';
+
+$data['h1'] = 'Форма заявки';
+
+$data['label_name'] = 'Ваше Имя';
+$data['label_email'] = 'Email адрес';
+$data['label_phone'] = 'Номер телефона';
+
+$data['button_send'] = 'Отправить';
+
+$data['error_name'] = 'Укажите ваше имя';
+$data['error_email'] = 'Укажите корректный Email';
+$data['error_phone'] = 'Укажите корректный телефон';
+$data['error_email_exist'] = 'Заявка с данным Email уже есть в базе';
+$data['error_phone_exist'] = 'Заявка с данным телефоном уже есть в базе';
+$data['error_request'] = 'Ошибка отправки. Попробуйте немного позже';
+
+$data['success'] = 'Ваша заявка успешно отправлена!';
+
